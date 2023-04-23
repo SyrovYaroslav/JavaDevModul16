ce08fb4076dbfc060bba2a3ac9dbbc0ae0338c17
Yaroslav <y.s.syrovatka@student.khai.edu>
2023-04-23 20:37:10 +0300

    ci prosses

---
+++ src/main/java/org/example/SumCalculator.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+public class SumCalculator {
+
+    public int sum(int n) {
+        if (n > 0) {
+            int result = 0;
+            for (int i = 1; i <= n; i++) {
+                result += i;
+            }
+            return result;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+}
